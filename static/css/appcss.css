44c3b6dad0bc4cf21ff92b7f5c8f8e0c6ccc9676
yix <1097444370@qq.com>
2018-01-01 00:27:28 +0800

    oks

---
+++ static/css/appcss.css
@@ -0,0 +1,23 @@
+.mtdatepicker{
+  color:rgb(65,65,65);
+  background: #fff;
+}
+
+.mint-datetime .picker-toolbar{
+  background:rgb(235,187,0);
+  color:#fff;
+}
+
+.mint-datetime-confirm{
+  color:#fff;
+}
+.mint-datetime-confirm.mint-datetime-action{
+  color:#fff !important;
+}
+.mint-datetime-cancel.mint-datetime-action{
+  color:#fff !important;
+}
+
+.picker-item{
+  color:rgb(235,187,0);
+}
